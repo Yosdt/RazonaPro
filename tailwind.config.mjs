34d026433a7d3bab64cc5c4bab73e1ee41dcbd8a
Yosdt <yddiazt@ufpso.edu.co>
2024-11-03 14:52:37 -0500

    Implemetacion Logo, tipografia

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			
+		},
 	},
 	plugins: [],
 }
